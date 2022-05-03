f98fe7a77d8da693e82600dbf7d0e556b101014e
Dahlia <20ftt1448@student.pb.edu.bn>
2022-05-03 21:23:35 +0800

    insert data into Terms and Conditions page

--- src/app/faq/faq.page.ts
+++ src/app/faq/faq.page.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { IonAccordionGroup } from '@ionic/angular';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.page.html',
   styleUrls: ['./faq.page.scss'],
 })
-export class FaqPage implements OnInit {
 
+export class FaqPage implements OnInit {
+  @ViewChild(IonAccordionGroup, { static: true }) accordionGroup: IonAccordionGroup;
   constructor() { }
 
   ngOnInit() {
-  }
-
+  }  
 }
